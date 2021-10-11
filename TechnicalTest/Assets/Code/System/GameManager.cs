51b05f1dfcb3d3f6522a1e468eb38d794e7c1a11
HarryHallows <hhallows96@gmail.com>
2021-10-11 14:01:18 +0100

    project setup

---
+++ TechnicalTest/Assets/Code/System/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private Scene currentScene;
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region ButtonFunctions
+
+    #region Menu Buttons
+    //Buttons specific to the menu scene
+    //Functions redirect you to correct location within project
+    public void RopeChallenge()
+    {
+        SceneManager.LoadScene(sceneName = "RopeChallenge");
+    }
+
+    public void FitnessChallenge()
+    {
+        SceneManager.LoadScene(sceneName = "FitnessChallenge");
+    }
+
+    public void CubeChallenge()
+    {
+        SceneManager.LoadScene(sceneName = "CubeChallenge");
+    }
+
+    #endregion
+
+    #endregion
+}
